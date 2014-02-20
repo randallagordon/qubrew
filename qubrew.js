18896d7f5e7ac832ae9aae0df7610521947637e0
Randall A. Gordon <randall@randallagordon.com>
2014-02-20 15:42:45 -0800

    Fix for crossplatform/various MIDI implementations

--- qubrew.js
+++ qubrew.js
@@ -3,13 +3,15 @@
 var sbmidi = require("spacebrew-midi"),
 	inPorts, outPorts
 
+function isQuNeo(portName) { return portName.indexOf("QUNEO") === -1 ? false : true; }
+
 sbmidi.connect({ name: "QuNeo" })
 
 inPorts  = sbmidi.getInPorts()
 outPorts = sbmidi.getOutPorts()
 
-sbmidi.openInPort(inPorts.indexOf("QUNEO"))
-sbmidi.openOutPort(outPorts.indexOf("QUNEO"))
+sbmidi.openInPort(inPorts.map(isQuNeo).indexOf(true))
+sbmidi.openOutPort(outPorts.map(isQuNeo).indexOf(true))
 
 // Ranges for first QuNeo mode
 sbmidi.addInputRange(0x24, 0x33)
