11773140e9df4fa07429372b929fbea5e536904d
Randall A. Gordon <randall@randallagordon.com>
2014-02-20 15:49:14 -0800

    Change Spacebrew description

--- qubrew.js
+++ qubrew.js
@@ -5,7 +5,7 @@ var sbmidi = require("spacebrew-midi"),
 
 function isQuNeo(portName) { return portName.indexOf("QUNEO") === -1 ? false : true; }
 
-sbmidi.connect({ name: "QuNeo" })
+sbmidi.connect({ name: "QuNeo", description: "QuNeo, via Spacebrew! Code on GitHub: github.com/randallagordon/qubrew and github.com/randallagordon/spacebrew-midi" })
 
 inPorts  = sbmidi.getInPorts()
 outPorts = sbmidi.getOutPorts()
